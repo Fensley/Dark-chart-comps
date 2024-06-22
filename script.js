55754d7257d9871c4ad4dd285d436cb6c6090306
Fensley <fortilusnehemie@gmail.com>
2024-06-22 12:58:16 -0400

    initial commit

--- script.js
+++ script.js
@@ -26,3 +26,68 @@ function drawChart() {
 
   chart.draw(data, options);
 }
+
+const home = document.getElementById("Home");
+const features = document.getElementById("Features");
+const pricing = document.getElementById("Pricing");
+const contact = document.getElementById("Contact");
+const sign = document.getElementById("Sign");
+const chart = document.querySelector(".chat");
+const thisis = document.querySelector(".thisis");
+const thisis1 = document.querySelector(".thisis1");
+const thish1 = document.getElementById("thish1").innerHTML;
+const all = document.querySelectorAll(".toggle");
+const valueone = document.getElementById("valueone");
+const valuetwo = document.getElementById("valuetwo");
+const inputV = document.getElementById("inputv");
+
+valuetwo.textContent = `$${inputV.value}`;
+
+inputV.addEventListener("input", (event) => {
+  valuetwo.textContent = `$ ${Math.round(event.target.value)}`;
+});
+
+function theven() {
+  contact.addEventListener("click", () => {
+    contact.style.color = "gray";
+    home.style.color = "white";
+    features.style.color = "white";
+    pricing.style.color = "white";
+    document.getElementById("thish1").innerHTML = "908-637-0326";
+    chart.style.display = "none";
+    thisis.style.visibility = "visible";
+  });
+
+  pricing.addEventListener("click", () => {
+    chart.style.display = "none";
+    thisis.style.visibility = "visible";
+    features.style.color = "white";
+    pricing.style.color = "gray";
+    home.style.color = "white";
+    contact.style.color = "white";
+    document.getElementById("thish1").innerHTML = "2999.00 usd";
+  });
+  features.addEventListener("click", () => {
+    contact.style.color = "white";
+
+    chart.style.display = "none";
+    thisis.style.visibility = "visible";
+    features.style.color = "gray";
+    home.style.color = "white";
+    pricing.style.color = "white";
+    document.getElementById("thish1").innerHTML = "this is features";
+  });
+
+  home.addEventListener("click", () => {
+    contact.style.color = "white";
+
+    chart.style.display = "block";
+    thisis.style.visibility = "hidden";
+
+    features.style.color = "white";
+    pricing.style.color = "white";
+    home.style.color = "gray";
+  });
+}
+
+theven();
